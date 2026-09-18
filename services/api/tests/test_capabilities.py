b623dfe35ad99eba739ba8cab735db605a8cf1ae
ccooper-data <coopercory.dataanalyst@gmail.com>
2026-09-17 20:44:49 -0500

    style: apply ruff import layout

--- services/api/tests/test_capabilities.py
+++ services/api/tests/test_capabilities.py
@@ -1,9 +1,5 @@
 from app.capabilities import evaluate_post_approval_capability
-from app.contracts import (
-    ApprovalArtifact,
-    CIValidationArtifact,
-    WorkflowRun,
-)
+from app.contracts import ApprovalArtifact, CIValidationArtifact, WorkflowRun
 
 
 SHA = "a" * 40
